c6e72c0c30b49b9d13c80fa85775cdbdd81bbcc8
mac-fc <mac.stamaria@flightcentre.com>
2024-10-19 20:44:42 +1000

    add hostname

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,6 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? DEFAULT_PORT);
+  await app.listen(process.env.PORT ?? DEFAULT_PORT, '0.0.0.0');
 }
 bootstrap();
