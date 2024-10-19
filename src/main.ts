f414e262513cd220505feaa3ae76fd3764582efc
mac-fc <mac.stamaria@flightcentre.com>
2024-10-19 15:25:59 +1000

    put port in .env and add default port

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const DEFAULT_PORT = 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT ?? 3000); // TODO: put port in ENV
+  await app.listen(process.env.PORT ?? DEFAULT_PORT);
 }
 bootstrap();
